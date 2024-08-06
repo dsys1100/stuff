9c680b85999680971cf697c3319826ca576b6010
dsys1100 <83178516+dsys1100@users.noreply.github.com>
2024-08-06 17:15:36 +0400

    fix

--- Linux/UpdDeb11,12to13_ROOT.sh
+++ Linux/UpdDeb11,12to13_ROOT.sh
@@ -4,5 +4,5 @@ sed -i 's/bullseye/trixie/g' /etc/apt/sources.list
 sed -i 's/bookworm/trixie/g' /etc/apt/sources.list
 sed -i 's/Bookworm/Trixie/g' /etc/apt/sources.list
 sed -i 's/Bullseye/Trixie/g' /etc/apt/sources.list
-apt full-upgrade -y && apt autoremove -y
+apt update && apt full-upgrade -y && apt autoremove -y
 reboot
