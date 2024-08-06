be3b65894740558d2dafed29b35b620979d1ffee
dsys1100 <83178516+dsys1100@users.noreply.github.com>
2024-08-06 17:16:05 +0400

    fix

--- Linux/UpdDeb11to12_ROOT.sh
+++ Linux/UpdDeb11to12_ROOT.sh
@@ -2,5 +2,5 @@
 apt update && apt upgrade -y
 sed -i 's/bullseye/bookworm/g' /etc/apt/sources.list
 sed -i 's/Bullseye/Bookworm/g' /etc/apt/sources.list
-apt full-upgrade -y && apt autoremove -y
+apt update && apt full-upgrade -y && apt autoremove -y
 reboot
