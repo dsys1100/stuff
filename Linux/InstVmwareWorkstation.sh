8c3b9cdb409b0daacf60a7daadb15499bb551be9
dsys1100 <danila.sysoev40@gmail.com>
2025-05-11 13:34:51 +0400

    Update InstVmwareWorkstation.sh

--- Linux/InstVmwareWorkstation.sh
+++ Linux/InstVmwareWorkstation.sh
@@ -3,10 +3,10 @@
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
 sudo apt install build-essential tar -y
 mkdir -p /tmp/vmw && cd /tmp/vmw/
-wget https://softwareupdate.vmware.com/cds/vmw-desktop/ws/17.6.1/24319023/linux/core/VMware-Workstation-17.6.1-24319023.x86_64.bundle.tar
-tar -xf VMware-Workstation-17.6.1-24319023.x86_64.bundle.tar
-chmod +x VMware-Workstation-17.6.1-24319023.x86_64.bundle
-sudo ./VMware-Workstation-17.6.1-24319023.x86_64.bundle
+wget https://web.archive.org/web/20250304135137/https://softwareupdate.vmware.com/cds/vmw-desktop/ws/17.6.3/24583834/linux/core/VMware-Workstation-17.6.3-24583834.x86_64.bundle.tar
+tar -xf VMware-Workstation-17.6.3-24583834.x86_64.bundle.tar
+chmod +x VMware-Workstation-17.6.3-24583834.x86_64.bundle
+sudo ./VMware-Workstation-17.6.3-24583834.x86_64.bundle
 sudo vmware-modconfig --console --install-all
 cd /tmp/
 rm -rf vmw
