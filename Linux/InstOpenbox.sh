f1c99d702312b108bfcb8fafc065641912898269
dsys1100testing <danila.sysoev40@gmail.com>
2025-07-02 11:44:42 +0300

    fix?

--- Linux/InstOpenbox.sh
+++ Linux/InstOpenbox.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "" > /etc/network/interfaces
+sudo echo "" > /etc/network/interfaces
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
 sudo apt install xorg openbox picom obconf tint2 menu dbus-x11 xfce4-terminal thunar lxappearance volumeicon-alsa alsa-utils network-manager network-manager-gnome pavucontrol pipewire-audio qt5-style-plugins mousepad gsimplecal xdg-utils nano curl wget -y
 echo "" >> .profile
